8933281188c62fd4d8c183a7fcd1269ea172fb77
Anastasiia Krasnoshapka <anastasiia.krasnoshapka@gmail.com>
2022-08-27 21:57:45 -0500

    minor improvements in methods

--- queries.js
+++ queries.js
@@ -1,55 +1,72 @@
-const priv = require('./private.env')
+require('dotenv').config()
 
-const Pool = require('pg').Pool
-const pool = new Pool({
-    user: priv.user,
-    host: priv.host,
-    database: priv.database,
-    password: priv.password,
-    port: priv.port,
+const { query } = require('express');
+const { Client } = require('pg')
+const client = new Client({
+    user: process.env.user,
+    host: process.env.host,
+    database: process.env.database,
+    password: process.env.password,
+    port: process.env.port,
 })
 
-const getUserRecords = (request, response) => {
-    const user_id = parseInt(request.params.user_id)
+client.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
 
-    pool.query('SELECT * FROM records WHERE user_id = $1', [user_id], (error, results) => {
-        if (error) {
-            throw error
+
+
+const getUserRecords = (request, response) => {
+    const user_id = parseInt(request.query.user_id)
+
+    client.query('SELECT * FROM records WHERE user_id = ' + user_id, (err, res) => {
+        if (err) {
+            console.log(err.stack)
+        } else {
+            response.status(200).json(res.rows)
         }
-        response.status(200).json(results.rows)
     })
 }
 
 const getUserDeviations = (request, response) => {
-    const user_id = parseInt(request.params.user_id)
+    const user_id = parseInt(request.query.user_id)
 
-    pool.query('SELECT deviations FROM records WHERE user_id = $1', [user_id], (error, results) => {
-        if (error) {
-          throw error
+    client.query('SELECT deviation FROM records WHERE user_id = $1', [user_id], (err, res) => {
+        if (err) {
+            console.log(err.stack)
+        } else {
+            response.status(200).json(res.rows)
         }
-        response.status(200).json(results.rows)
     })
 }
 
 const createRecord = (request, response) => {
-    const { label, filepath, user_id, created_at, deviation } = request.body
-  
-    pool.query('INSERT INTO records (label, filepath, user_id, created_at, deviation) VALUES ($1, $2, $3, $4, $5) RETURNING *', [label, filepath, user_id, created_at, deviation], (error, results) => {
-        if (error) {
-            throw error
+    const label = request.query.label
+    const filepath = request.query.filepath
+    const user_id = parseInt(request.query.user_id)
+    const created_at = request.query.user_id
+    const deviation = parseInt(request.query.deviation)
+
+    client.query('INSERT INTO public.records(label, filepath, user_id, created_at, deviation) VALUES ($1, $2, $3, $4, $5)', 
+                    [label, filepath, user_id, created_at, deviation], (err, res) => {
+        if (err) {
+            console.log(err.stack)
+        } else {
+            response.status(200).json(res.rows)
         }
-        response.status(201).send(`Record added with ID: ${results.rows[0].id}`)
     })
 }
 
 const deleteRecord = (request, response) => {
-    const user_id = parseInt(request.params.user_id)
-  
-    pool.query('DELETE FROM records WHERE id = $1', [user_id], (error, results) => {
-        if (error) {
-            throw error
+    const id = parseInt(request.query.id)
+
+    client.query('DELETE FROM records WHERE id = $1', [id], (err, res) => {
+        if (err) {
+            console.log(err.stack)
+        } else {
+            response.status(200).json(res.rows)
         }
-        response.status(200).send(`Record deleted with ID: ${user_id}`)
     })
 }
 
